6bd99a23e166f55c1bd21d861fb8f9b8001445b7
Akshay Shrivastava <akshayshrivastava@Akshays-MacBook-Pro.local>
2021-06-19 12:40:50 +0530

    Rename tarun sqad to tarun

--- tarun/tarun/ecr_program/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "terraform-tarun-squad-km"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
