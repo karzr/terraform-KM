c4745c6875659ab70ca4cc7018e8f6b1aba24364
Akshay Shrivastava <akshayshrivastava@Akshays-MacBook-Pro.local>
2021-06-19 12:27:50 +0530

    added ecr program for akshay

---
+++ tarun_squad/akshay/ecr_program/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_ecr_repository" "foo" {
+  name                 = "/tarun/akshay_repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
